2159656c17bdf4ae8442f40faa264f5e5e3a545b
Ahsan Ali <ahsanriaz0475@gmail.com>
2026-03-09 16:11:53 +0500

    Initial commit - Vite + React + Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-black">
+        Tailwind is Working!
+      </h1>
+    </>
+  );
+}
+
+export default App;
